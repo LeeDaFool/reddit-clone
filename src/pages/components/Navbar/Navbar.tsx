7c6a4c6ff8188168bb1de1b6cc61e17ee77a0341
Chang Metellus <metellus.chang@gamil.com>
2023-04-15 00:02:33 -0400

    finish directory on navbar

--- src/pages/components/Navbar/Navbar.tsx
+++ src/pages/components/Navbar/Navbar.tsx
@@ -1,21 +1,26 @@
-import { Flex, Image } from '@chakra-ui/react'
-import React from 'react'
-import SearchInput from './SearchInput'
-import RightContent from './RightContent/RightContent'
-import { useAuthState } from 'react-firebase-hooks/auth'
-import { auth } from '@/src/firebase/clientApp'
+import { Flex, Image } from "@chakra-ui/react";
+import React from "react";
+import SearchInput from "./SearchInput";
+import RightContent from "./RightContent/RightContent";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/src/firebase/clientApp";
+import Directory from "./Directory/Directory";
 
 export default function Navbar({}) {
   const [user, loading, error] = useAuthState(auth);
   return (
-    <Flex bg='white' height='44px' padding='6px 12px'>
-      <Flex align='center'>
-        <Image src="/images/redditFace.svg" height='30px' />
-        <Image src="/images/redditText.svg" height='46px' display={{ base: 'none', md: "unset" }} />
+    <Flex bg="white" height="44px" padding="6px 12px" justify={{ md: 'space-between'}}>
+      <Flex align="center" width={{ base: '40px', md: 'auto'}} mr={{ base: 0, md: 2}}>
+        <Image src="/images/redditFace.svg" height="30px" />
+        <Image
+          src="/images/redditText.svg"
+          height="46px"
+          display={{ base: "none", md: "unset" }}
+        />
       </Flex>
-      {/* <Directory /> */}
-      <SearchInput />
+      {user && <Directory />}
+      <SearchInput user={user} />
       <RightContent user={user} />
     </Flex>
-  )
+  );
 }
--- src/pages/components/Navbar/RightContent/UserMenu.tsx
+++ src/pages/components/Navbar/RightContent/UserMenu.tsx
@@ -8,39 +8,109 @@ import {
   MenuItem,
   Icon,
   Flex,
+  MenuDivider,
+  Box,
+  Text
 } from "@chakra-ui/react";
-import { User } from "firebase/auth";
+import { User, signOut } from "firebase/auth";
 import { FaRedditSquare } from "react-icons/fa";
 import { VscAccount } from "react-icons/vsc";
 import { IoSparkles } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+import { MdOutlineLogin } from "react-icons/md";
+import { auth } from "@/src/firebase/clientApp";
+import { useSetRecoilState } from "recoil";
+import { authModalState } from "@/src/atoms/authModalAtom";
 
 type UserMenuProps = {
   user?: User | null;
 };
 
 const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
+  const setAuthModalState = useSetRecoilState(authModalState);
+
   return (
     <Menu>
-      <MenuButton>
-        {user ? (
-          <Flex align='center'>
-            <Flex align='center'>
-            <>
-              <Icon fontSize={24} mr={1} color="gray.300" as={FaRedditSquare} />
-            </>
-            <ChevronDownIcon />
-            </Flex>
+      <MenuButton
+        cursor="pointer"
+        padding="0px 6px"
+        borderRadius={4}
+        _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
+      >
+        <Flex align="center">
+          <Flex align="center">
+            {user ? (
+              <>
+                <Icon
+                  fontSize={24}
+                  mr={1}
+                  color="gray.300"
+                  as={FaRedditSquare}
+                />
+                <Box
+                  display={{ base: "none", lg: "flex" }}
+                  flexDirection="column"
+                  fontSize="8pt"
+                  alignItems="flex-start"
+                  mr={8}
+                >
+                  <Text fontWeight={700}>
+                    {user?.displayName || user?.email?.split("@")[0]}
+                  </Text>
+                  <Flex alignItems="center">
+                    <Icon as={IoSparkles} color="brand.100" mr={1} />
+                    <Text color="gray.400">1 karma</Text>
+                  </Flex>
+                </Box>
+              </>
+            ) : (
+              <Icon fontSize={24} color="gray.400" mr={1} as={VscAccount} />
+            )}
           </Flex>
-        ) : (
-          <div>no user</div>
-        )}
+          <ChevronDownIcon />
+        </Flex>
       </MenuButton>
       <MenuList>
-        <MenuItem>Download</MenuItem>
-        <MenuItem>Create a Copy</MenuItem>
-        <MenuItem>Mark as Draft</MenuItem>
-        <MenuItem>Delete</MenuItem>
-        <MenuItem>Attend a Workshop</MenuItem>
+        {user ? (
+          <>
+            <MenuItem
+              fontSize="10pt"
+              fontWeight={700}
+              _hover={{ bg: "blue.500", color: "white" }}
+            >
+              <Flex align="center">
+                <Icon fontSize={20} mr={2} as={CgProfile} />
+                Profile
+              </Flex>
+            </MenuItem>
+            <MenuDivider />
+            <MenuItem
+              fontSize="10pt"
+              fontWeight={700}
+              _hover={{ bg: "blue.500", color: "white" }}
+              onClick={() => signOut(auth)}
+            >
+              <Flex align="center">
+                <Icon fontSize={20} mr={2} as={MdOutlineLogin} />
+                Log Out
+              </Flex>
+            </MenuItem>
+          </>
+        ) : (
+          <>
+            <MenuItem
+              fontSize="10pt"
+              fontWeight={700}
+              _hover={{ bg: "blue.500", color: "white" }}
+              onClick={() => setAuthModalState({ open: true, view: "login" })}
+            >
+              <Flex align="center">
+                <Icon fontSize={20} mr={2} as={MdOutlineLogin} />
+                Log In / Sign Up
+              </Flex>
+            </MenuItem>
+          </>
+        )}
       </MenuList>
     </Menu>
   );
