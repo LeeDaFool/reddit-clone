02b87c24bd5b6d230ef14203085d08ca87ff8fb7
Chang Metellus <metellus.chang@gamil.com>
2023-06-27 15:18:31 -0400

    fix build error

--- src/pages/components/Navbar/SearchInput.tsx
+++ src/pages/components/Navbar/SearchInput.tsx
@@ -9,11 +9,9 @@ type SearchInputProps = {
 
 const SearchInput: React.FC<SearchInputProps> = ({ user }) => {
   return (
-    <Flex flexGrow={1} maxWidth={user ? 'auto' : '600px'} mr={2} align="center">
+    <Flex flexGrow={1} maxWidth={user ? "auto" : "600px"} mr={2} align="center">
       <InputGroup>
-        <InputLeftElement
-          pointerEvents="none"
-        >
+        <InputLeftElement pointerEvents="none">
           <SearchIcon color="gray.400" mb={1} />
         </InputLeftElement>
         <Input
--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -20,17 +20,16 @@ const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
   console.log("here is data", communityData);
   const setCommunityStateValue = useSetRecoilState(communityState);
 
-  if (!communityData) {
-    return <NotFound />;
-  }
-
   useEffect(() => {
     setCommunityStateValue((prev) => ({
       ...prev,
       currentCommunity: communityData,
     }));
-  }, [setCommunityStateValue, communityData]);
+  }, [communityData]);
 
+  if (!communityData) {
+    return <NotFound />;
+  }
   return (
     <>
       <Header communityData={communityData} />
