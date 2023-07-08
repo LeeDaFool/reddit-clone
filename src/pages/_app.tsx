be634041be3bb2c90682c1eb2bc362f584205fdb
Chang Metellus <metellus.chang@gamil.com>
2023-07-08 17:13:09 -0400

    this the one right here

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
-import { theme } from "./components/chakra/theme";
+import { theme } from "../chakra/theme";
 import Layout from "./components/Layout/Layout";
 import { RecoilRoot } from "recoil";
 
